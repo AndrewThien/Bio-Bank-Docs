701829b855e1e0296bf7f4e97cf1e1cd2adccc88
Tri Thien Nguyen <anretrithien@gmail.com>
2023-12-19 21:37:29 +0000

    Make the layout for the docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Bio Bank</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/AndrewThien/Bio-Bank',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://www.linkedin.com/in/tri-thien-nguyen/',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/AndrewThien/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Made by Tri Thien Nguyen',
   },
 }
 
